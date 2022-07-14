0916a8609bc4bf86d6e131a52daee1b50a1d676d
Manel2004 <71586983+Manel2004@users.noreply.github.com>
2022-07-14 10:11:44 +0200

    Update product_tables.php

--- product_tables.php
+++ product_tables.php
@@ -2,8 +2,11 @@ es (6 sloc) 88 Bytes
 <?php
 
 for ($i = 1; $i <= 10; $i++) {
+	echo "               ".PHP_EOL;
+	echo "Taula del "."$i".PHP_EOL;
+	echo "-----------------".PHP_EOL;
 	for ($n = &$i, $n <= 10, $n ++) {
-		echo "$n".PHP_EOL
-} 
+		echo "$i"." x "."$n"." = ".$n * $i.PHP_EOL;
+    } 
 }
 
