fc0cba0e7c786eda748cbe0bceb6dc1163494c82
Manel2004 <71586983+Manel2004@users.noreply.github.com>
2022-07-14 10:17:32 +0200

    Update product_tables.php

--- product_tables.php
+++ product_tables.php
@@ -5,7 +5,7 @@ for ($i = 1; $i <= 10; $i++) {
 	echo "               ".PHP_EOL;
 	echo "Taula del "."$i".PHP_EOL;
 	echo "-----------------".PHP_EOL;
-	for ($n = &$i; $n <= 10; $n ++) {
+	for ($n = 1; $n <= 10; $n ++) {
 		echo "$i"." x "."$n"." = ".$n * $i.PHP_EOL;
     } 
 }
